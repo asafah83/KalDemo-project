09a141d0ee3b52461a1e753388f76fc1c1a6bb43
Asaf Aharoni <asaf.aharoni@covercy.com>
2025-02-16 20:43:11 +0200

    changes update 5

--- src/test/java/tests/E2Etests.java
+++ src/test/java/tests/E2Etests.java
@@ -14,27 +14,23 @@ public class E2Etests extends BaseTest {
     @Test
     @Description("Test 1 - Login with a valid user, to the website and Create a new Ad")
     private void e2eTest_loginAndCreateAd() throws InterruptedException {
-        HomePage homePage = new HomePage(driver);
-        Step1_page step1_page = new Step1_page(driver);
-        Step2_page step2_page = new Step2_page(driver);
-        Step3_page step3_page = new Step3_page(driver);
-        Step4_page step4_page = new Step4_page(driver);
-        Step5_page step5_page = new Step5_page(driver);
-        Step6_page step6_page = new Step6_page(driver);
-        AssetsInventoryPage assetsInventoryPage = new AssetsInventoryPage(driver);
 
         // Starting the E2E test by beginning the publishing flow
+        HomePage homePage = new HomePage(driver);
+        AssetsInventoryPage assetsInventoryPage = new AssetsInventoryPage(driver);
         homePage.selectTabByName("מאגר דירות");
         assetsInventoryPage.countAdsBefore();
         homePage.clickOnPublishAdButton();
 
-        // Go over on the first step of the wizard
+        // Proceed with the first step of the wizard
+        Step1_page step1_page = new Step1_page(driver);
         step1_page.selectAssetType("דירת גן");
         step1_page.selectAssetStatus("משופץ");
         step1_page.selectCityAndHandleStreet("כרמיאל", "אשור", "24");
         step1_page.clickNext1();
 
-        // Go over on the second step of the wizard
+        // Proceed with the second step of the wizard
+        Step2_page step2_page = new Step2_page(driver);
         step2_page.setFloor("1");
         step2_page.setFromFloor("2");
         step2_page.selectNumberOfRooms("4");
@@ -44,14 +40,16 @@ public class E2Etests extends BaseTest {
         step2_page.setElevator("עם");
         step2_page.clickNext2();
 
-        // Go over on the third step of the wizard
+        // Proceed with the third step of the wizard
+        Step3_page step3_page = new Step3_page(driver);
         step3_page.selectAssetAttributeByName("גישה לנכים");
         step3_page.selectAssetAttributeByName("מטבח כשר");
         step3_page.selectAssetAttributeByName("דוד שמש");
         step3_page.setAssetDescription("בדיקה 1 בדיקה 23");
         step3_page.clickNext3();
 
-        // Go over on the fourth step of the wizard
+        // Proceed with the fourth step of the wizard
+        Step4_page step4_page = new Step4_page(driver);
         step4_page.setNumberOfPayments("12");
         step4_page.setHouseCommitment("200");
         step4_page.setTaxAsset("600");
@@ -59,11 +57,13 @@ public class E2Etests extends BaseTest {
         step4_page.selectDate("2025", "November", "29");
         step4_page.clickNext4button();
 
-        // Go over on the fifth step of the wizard
+        // Proceed with the fifth step of the wizard
+        Step5_page step5_page = new Step5_page(driver);
         step5_page.uploadImage("C:\\Automation\\folderForTestImages\\UK.jpg");
         step5_page.clickNext5Button();
 
-        // Go over on the sixth step of the wizard
+        // Proceed with the sixth step of the wizard
+        Step6_page step6_page = new Step6_page(driver);
         step6_page.setFullName("Asaf test");
         step6_page.setPhoneNumber("0523213214");
         step6_page.clickThePublishButton();
